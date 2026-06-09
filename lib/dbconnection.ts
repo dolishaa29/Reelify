9658c3156d44b28a7c867510d44410d717793cb1
dolishaa29 <dolishagandhi@gmail.com>
2026-06-10 00:50:09 +0530

    first commit

--- lib/dbconnection.ts
+++ lib/dbconnection.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { buffer } from "stream/consumers";
 
 const MONGO_URL=process.env.MONGO_URI!
 if(!MONGO_URL)
@@ -7,3 +8,36 @@ if(!MONGO_URL)
 }
 
 let cached=global.mongoose
+
+if(!cached)
+{
+    cached=global.mongoose={conn:null , promise:null}
+}
+
+export async function connectToDatabse(){
+    if(cached.conn)
+    {
+        return cached.conn
+    }
+    if(!cached.promise)
+    {
+        const opts={
+            bufferCommands:true,
+            maxPoolSize:10
+        }
+        mongoose.connect(MONGO_URL,opts)
+        .then(()=>mongoose.connection)
+    }
+
+    try{
+       cached.conn= await cached.promise
+       
+    }
+    catch(error)
+    {
+        cached.promise=null;
+        throw error
+    }
+
+    return cached.conn
+}
