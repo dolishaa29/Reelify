ff12167b3c9a157f5719a7cf83738cc4fbb3d408
dolishaa29 <dolishagandhi@gmail.com>
2026-06-10 11:52:36 +0530

    next-auth

---
+++ lib/auth.ts
@@ -0,0 +1,76 @@
+import { NextAuthOptions } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { connectToDatabse } from "./dbconnection";
+import User from "@/models/user";
+import bcrypt from "bcryptjs";
+
+export const authOptions :NextAuthOptions ={
+  providers:[
+    Credentials({
+        name:"Credentials",
+        credentials:{
+            email:{label:"Email" , type:"text"},
+            password:{label:"Password", type:"password"}
+        },
+        async authorize(credentials)
+        {
+         if(!credentials?.email || !credentials?.password)
+         {
+            throw new Error("Missing email or password")
+         }
+
+         try{
+            await connectToDatabse()
+            const user=await User.findOne({email:credentials.email})
+
+            if(!user)
+            {
+                throw new Error("No user found with this");
+            }
+
+            const isValid=await bcrypt.compare(
+                credentials.password,
+                user.password
+            )
+
+            if(!isValid)
+            {
+                throw new Error("invalid password");
+            }
+
+            return{
+                id:user._id.toString(),
+                email:user.email
+            }
+         }
+         catch(error)
+         {
+            console.error("Auth error")
+            throw error
+         }
+
+        }
+    })
+   
+  ],
+
+  callbacks:{
+    async jwt({token,user})
+    {
+        if(user)
+        {
+            token._id=user.id
+        }
+        return token
+    },
+    async session({session  ,token})
+    {
+        if(session.user)
+        {
+            session.user.id=token.id as string
+        }
+        return session;
+    },
+  },
+};
+ 
