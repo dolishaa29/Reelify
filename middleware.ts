cecde8df06579c54f42719aa73d7e179b003e8d7
dolishaa29 <dolishagandhi@gmail.com>
2026-06-15 13:11:26 +0530

    middleware

---
+++ middleware.ts
@@ -0,0 +1,30 @@
+import {withAuth} from "next-auth/middleware"
+import { NextResponse } from "next/server"
+import path from "path";
+import { callbackify } from "util";
+
+export default withAuth(
+    function middleware()
+    {
+        return NextResponse.next();
+    },
+    {
+        callbacks:{
+            authorized({req,token})
+            {
+                const {pathname}=req.nextUrl
+                if(
+                    pathname.startsWith("/api/auth") || pathname==="/login" || pathname==="/register"
+                )
+                return true;
+
+                if(pathname==="/" || pathname.startsWith("/api/videos"))
+                {
+                    return true;
+                }
+
+                return !!token
+            }
+        }
+    }
+)
